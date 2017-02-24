3e316b1f4d4dd1b41b0cd4764bcb614f108e03cc
Manuel Parra <manuparra@gmail.com>
2017-02-24 19:08:00 +0100

    Changes added

---
+++ MinReducer.java
@@ -0,0 +1,30 @@
+package oldapi;
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+
+public class MinReducer extends MapReduceBase implements Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+	
+		// Funcion Reduce:		
+		public void reduce(Text key, Iterator<DoubleWritable> values, OutputCollector<Text, DoubleWritable> output, Reporter reporter) throws IOException {
+
+		// Para extraer el Minimo usamos de valor incial el máximo de JAVA
+		Double minValue = Double.MAX_VALUE;
+		
+		// Leemos cada tupla  y nos quedamos con el menor valor
+		while (values.hasNext()) {
+			minValue = Math.min(minValue, values.next().get());
+		}
+		
+		// Hacemos el collect con la key el valor mínimo encontrado en esta fase de reducción
+		output.collect(key, new DoubleWritable(minValue));
+	}
+}
+
